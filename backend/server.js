60279912687b70c6ffd6c4b8fcebbf356609326e
Fenil <201901130@daiict.ac.in>
2024-01-25 22:21:01 +0530

    make changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -29,19 +29,20 @@ app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static('/var/data/uploads'));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '/frontend/build')));
-
-    app.get('*', (req, res) => 
-        res.sendFile(path.resolve(__dirname, 'frontend','build', 'index.html'))
-    );
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
 } else {
-    app.get('/',(req, res) => {
-        res.send('Api is running...');
-    })
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
 }
 
 app.use(notFound);
