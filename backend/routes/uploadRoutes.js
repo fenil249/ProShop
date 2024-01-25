a3a15d57adafcedce8015e946ca6f2207a36cb44
Fenil <201901130@daiict.ac.in>
2024-01-25 13:42:15 +0530

    make changes for upload images

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,49 +1,61 @@
-import path from 'path';
-import express from 'express';
-import multer from 'multer';
+import path from "path";
+import express from "express";
+import multer from "multer";
+import sharp from "sharp";
 
 const router = express.Router();
 
 const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename(req, file, cb) {
-    cb(
-      null,
-      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-    );
-  },
+    destination(req, file, cb){
+        cb(null,'uploads/');
+    },
+    filename(req, file, cb){
+        cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
+    }
 });
 
-function fileFilter(req, file, cb) {
-  const filetypes = /jpe?g|png|webp/;
-  const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
-
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = mimetypes.test(file.mimetype);
-
-  if (extname && mimetype) {
-    cb(null, true);
-  } else {
-    cb(new Error('Images only!'), false);
-  }
+function checkFileType(file, cb){
+    const filetypes = /jpg|jpeg|png/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
+    if(extname && mimetype){
+        return cb(null, true);
+    } else {
+        cb('Images only!');
+    }
 }
 
-const upload = multer({ storage, fileFilter });
-const uploadSingleImage = upload.single('image');
+const upload = multer({
+    storage,
+})
 
-router.post('/', (req, res) => {
-  uploadSingleImage(req, res, function (err) {
-    if (err) {
-      return res.status(400).send({ message: err.message });
+router.post('/', upload.single('image'), async (req,res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).send('No file uploaded.');
+        }
+
+        const width = 640;
+        const height = 510;
+
+        await sharp(req.file.path)
+            .resize({
+                width,
+                height,
+                fit: 'fill',
+                position: 'center'
+            })
+            .toFile(`uploads/downscaled-${req.file.filename}`);
+
+        res.send({
+            message: 'Image uploaded',
+            image: `/uploads/downscaled-${req.file.filename}`
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('An error occurred during image processing.');
     }
-
-    res.status(200).send({
-      message: 'Image uploaded successfully',
-      image: `/${req.file.path}`,
-    });
-  });
-});
+    
+})
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,24 @@
 import path from 'path';
-import express from 'express'
-import dotenv from 'dotenv'
-import productRoutes from './routes/productRoutes.js'
+import express from 'express';
+import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+dotenv.config();
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
-dotenv.config();
-import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-const port=process.env.PORT || 5000;
+
+const port = process.env.PORT || 5000;
 
 connectDB();
-const app=express();
+
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -28,25 +29,24 @@ app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-if (process.env.NODE_ENV === 'production') {
-  const __dirname = path.resolve();
-  app.use('/uploads', express.static('/var/data/uploads'));
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
+    app.get('*', (req, res) => 
+        res.sendFile(path.resolve(__dirname, 'frontend','build', 'index.html'))
+    );
 } else {
-  const __dirname = path.resolve();
-  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
+    app.get('/',(req, res) => {
+        res.send('Api is running...');
+    })
 }
-
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port,()=>console.log(`server is running on port ${port}`));
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
